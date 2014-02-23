8cb691c2e16ae6e3aba9e7521f532f7f056eafb5
Yaug <manuel.esteban@chroniques-lunaires.com>
2014-02-23 13:51:32 +0100

    start the bundle

---
+++ Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+// src/Ydle/IhmBundle/Entity/User.php
+
+namespace Ydle\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setRoles(array(static::ROLE_DEFAULT));
+        // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
